fcc1226d5b993380907b2d839480ee43816d4093
Fulgus <santosjosemaria9@gmail.com>
2018-05-11 12:59:01 +0200

    Añadido modificar perfil, creado el bean de perfil vacio.

---
+++ PCeraJSF/src/java/es/uma/informatica/sii/jsf/autenticacion/PerfilBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.uma.informatica.sii.jsf.autenticacion;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author JoseMaria
+ */
+@Named(value = "perfil")
+@RequestScoped
+public class PerfilBean {
+
+    /**
+     * Creates a new instance of PerfilBean
+     */
+    public PerfilBean() {
+    }
+    
+}
